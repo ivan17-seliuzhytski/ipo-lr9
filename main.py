c65bce8451de41fa77f2d3900569f50ae069e128
Ivan <ivan.seliuzhytski17@gmail.com>
2024-12-12 14:34:32 +0300

    Task 6 complete

---
+++ main.py
@@ -0,0 +1,6 @@
+from collision import second, third, fourth, fifth
+second.isCorrectRect()
+third.isCollisionRect() 
+fourth.intersectionAreaRect()
+rectangles = []
+fifth.intersectionAreaMultiRect(rectangles)
