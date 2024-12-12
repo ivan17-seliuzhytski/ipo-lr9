c8261cacd3dbbbe90d654aa8ef44529115d7d452
Ivan <ivan.seliuzhytski17@gmail.com>
2024-12-12 10:34:46 +0300

    Task 6 complete

---
+++ main.py
@@ -0,0 +1,7 @@
+
+from collision import isCollisionRect, isCorrectRect, intersectionAreaRect, intersectionAreaMultiRect
+isCollisionRect.isCorrectRect()
+isCorrectRect.isCollisionRect() 
+intersectionAreaRect.intersectionAreaRect()
+rectangles = []
+intersectionAreaMultiRect.intersectionAreaMultiRect(rectangles)
