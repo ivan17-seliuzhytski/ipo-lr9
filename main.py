519e5959b1750ebe4064ef36bddacdec51f4fd21
Ivan <ivan.seliuzhytski17@gmail.com>
2024-12-12 14:34:11 +0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-
-from collision import second, third, fourth, fifth
-second.isCorrectRect()
-third.isCollisionRect() 
-fourth.intersectionAreaRect()
-rectangles = []
-fifth.intersectionAreaMultiRect(rectangles)
